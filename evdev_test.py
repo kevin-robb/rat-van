63a6133f8254e55a1511878741b62d1a4cedbf64
Kevin Robb <kevin.robb@alumni.ou.edu>
2021-08-12 15:48:43 -0500

    Testing evdev instead of pygame for joystick input. Can verify evdev is installed but running the python file gives error that the module is not found.

---
+++ evdev_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import sys
+import time
+from evdev import InputDevice, list_devices, ecodes, categorize
+
+print("Press Ctrl-C to Quit")
+time.sleep(1)
+
+devices = [InputDevice(path) for path in list_devices()]
+for dev in devices:
+    print("Input Device Found: " + dev.name)
+
+if not devices:
+    print('No Joysticks Found. Quitting.')
+    exit()
+
+# create gamepad object
+gp = InputDevice('/dev/input/event3')
+# print device info
+print(gp)
+
+# display codes
+for event in gp.read_loop():
+    # buttons
+    if event.type == ecodes.EV_KEY:
+        print(event)
+    # analog
+    elif event.type == ecodes.EV_ABS:
+        absevent = categorize(event)
+        print(ecodes.bytype[absevent.event.type][absevent.event.code], absevent.event.value)
+
+
+
+# try:
+    
+# except KeyboardInterrupt:
+#     sys.exit()
